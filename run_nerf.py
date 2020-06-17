ff79dfb64188a74dd84695287140b0787875f1fa
Jing Yang <jingyang.carl.work@gmail.com>
2020-06-17 14:03:28 -0700

    add depth map in render()

--- run_nerf.py
+++ run_nerf.py
@@ -333,7 +333,8 @@ def render(H, W, focal,
         k_sh = list(sh[:-1]) + list(all_ret[k].shape[1:])
         all_ret[k] = tf.reshape(all_ret[k], k_sh)
 
-    k_extract = ['rgb_map', 'disp_map', 'acc_map']
+    # k_extract = ['rgb_map', 'disp_map', 'acc_map']
+    k_extract = ['rgb_map', 'disp_map', 'acc_map', 'depth_map']
     ret_list = [all_ret[k] for k in k_extract]
     ret_dict = {k: all_ret[k] for k in all_ret if k not in k_extract}
     return ret_list + [ret_dict]
